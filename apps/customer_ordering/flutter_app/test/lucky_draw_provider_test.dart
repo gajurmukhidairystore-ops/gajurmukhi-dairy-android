f4798cec749a3e87fada8358fd80bf1e3f6f98cf
gajurmukhidairystore-ops <3.17128159e+08+gajurmukhidairystore-ops@users.noreply.github.com>
2026-08-16 08:54:53 +0000

    Clean lucky draw integration test imports

--- apps/customer_ordering/flutter_app/test/lucky_draw_provider_test.dart
+++ apps/customer_ordering/flutter_app/test/lucky_draw_provider_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:gajurmukhi_dairy_business_pro/data/database.dart';
 import 'package:gajurmukhi_dairy_business_pro/providers/business_provider.dart';
